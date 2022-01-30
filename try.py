2373c03b77bb38cc3e841f8c7c5b673325f76f52
Maxteal <84003531+Maxteal@users.noreply.github.com>
2022-01-30 22:40:03 +0300

    Update try.py

--- try.py
+++ try.py
@@ -1,13 +1,22 @@
 import requests
+from time import sleep
 
-payload = {
-   'content': "!mine"
-    
+count = 0
+
+
+
+while count < 5:
+
+
+    payload = {
+        'content': "!mine"
 }
 
-header = {
-
-    'authorization': ''
+    header = {
+        'authorization': 'Your token here'
 }
 
-r = requests.post("https://discord.com/api/v9/channels/923994324453179443/messages", data=payload, headers=header)
+    r = requests.post("https://discord.com/api/v9/channels/923994324453179443/messages", data=payload, headers=heade>
+
+
+    sleep(3)
